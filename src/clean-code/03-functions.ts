fd2c3a9d240708288d5000d9c5ebc53556a2ea18
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-07 22:29:07 +0200

    ById is a good convention, as Mozilla does

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,16 +1,16 @@
 ;(() => {
   // función para obtener información de una película por Id
-  function getMovieInfo(movieId: string) {
-    console.log({ movieId })
+  function getMovieInfoById(id: string) {
+    console.log({ id })
   }
 
   // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-  function getMovieActorsInfo(movieId: string) {
-    console.log({ movieId })
+  function getMovieActorsInfoById(id: string) {
+    console.log({ id })
   }
 
   // funcion para obtener el bio del actor por el id
-  function getActorBio(id: string) {
+  function getActorBioById(id: string) {
     console.log({ id })
   }
 
