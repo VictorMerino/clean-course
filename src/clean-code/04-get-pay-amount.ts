c82e4e4a4f92dfc96d6d3a3f28e0a9493a181f1d
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-07 22:58:34 +0200

    Better one liners when is clear this way

--- src/clean-code/04-get-pay-amount.ts
+++ src/clean-code/04-get-pay-amount.ts
@@ -3,14 +3,11 @@ const getPayAmount = ({
   isSeparated = true,
   isRetired = false
 }): number => {
-  if (isDead) {
-    return 1500
-  }
-  if (isSeparated) {
-    return 2500
-  }
-  if (isRetired) {
-    return 3000
-  }
+  if (isDead) return 1500
+
+  if (isSeparated) return 2500
+
+  if (isRetired) return 3000
+
   return 4000
 }
