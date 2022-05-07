8c23dccd4ac907808ff22991dcf22950f2e1fcb8
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-07 22:26:51 +0200

    Functions

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,37 @@
+;(() => {
+  // función para obtener información de una película por Id
+  function getMovieInfo(movieId: string) {
+    console.log({ movieId })
+  }
+
+  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+  function getMovieActorsInfo(movieId: string) {
+    console.log({ movieId })
+  }
+
+  // funcion para obtener el bio del actor por el id
+  function getActorBio(id: string) {
+    console.log({ id })
+  }
+
+  // Crear una película
+  function createMovie(
+    title: string,
+    description: string,
+    rating: number,
+    cast: string[]
+  ) {
+    console.log({ title, description, rating, cast })
+  }
+
+  // Crea un nuevo actor
+  function createActor(fullName: string, birthdate: Date): boolean {
+    // tarea asincrona para verificar nombre
+    // ..
+    // ..
+    if (fullName === 'fernando') return false
+
+    console.log('Crear actor')
+    return true
+  }
+})()
