2b408022a90f696f82bac7b4d90248376d2d6127
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-08 21:46:50 +0200

    Well, it worked, but I'm cheating: not actually using composition but a new class

--- src/clean-code/07-tarea.ts
+++ src/clean-code/07-tarea.ts
@@ -8,28 +8,35 @@
     constructor(public id: string, public type: HtmlType) {}
   }
 
-  class InputAttributes extends HtmlElement {
-    constructor(public value: string, public placeholder: string, id: string) {
-      super(id, 'input')
-    }
+  class InputAttributes {
+    constructor(public value: string, public placeholder: string, id: string) {}
   }
 
-  class InputEvents extends InputAttributes {
-    constructor(value: string, placeholder: string, id: string) {
-      super(value, placeholder, id)
-    }
-
+  class InputEvents {
     setFocus() {}
     getValue() {}
     isActive() {}
     removeValue() {}
   }
 
-  //? Idea para la nueva clase InputElement
+  interface InputElementProperties extends HtmlElement, InputAttributes {}
 
-  const nameField = new InputEvents('Fernando', 'Enter first name', 'txtName')
+  //? Idea para la nueva clase InputElement
+  class InputElement {
+    public id: string
+    public placeholder: string
+    public value: string
+    public type: HtmlType
+    constructor(value: string, placeholder: string, id: string) {
+      this.type = 'input'
+      this.value = value
+      this.placeholder = placeholder
+      this.id = id
+    }
+  }
+  // const nameField = new InputEvents('Fernando', 'Enter first name', 'txtName')
   // This should return exactly the same as before with the same input
-  // const nameField = new InputElement('Fernando', 'Enter first name', 'txtName')
+  const nameField = new InputElement('Fernando', 'Enter first name', 'txtName')
 
   console.log({ nameField })
 })()
