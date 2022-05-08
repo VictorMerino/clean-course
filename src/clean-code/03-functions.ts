b68168aa3c8982d1fa54a1e79107e5fbb244cdba
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-08 11:47:34 +0200

    Move getPayAmount method to functions file

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -39,4 +39,18 @@
     console.log('Crear actor')
     return personalData
   }
+
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false
+  }): number => {
+    if (isDead) return 1500
+
+    if (isSeparated) return 2500
+
+    if (isRetired) return 3000
+
+    return 4000
+  }
 })()
--- src/clean-code/04-get-pay-amount.ts
+++ src/clean-code/04-get-pay-amount.ts
@@ -1,13 +1,1 @@
-const getPayAmount = ({
-  isDead = false,
-  isSeparated = true,
-  isRetired = false
-}): number => {
-  if (isDead) return 1500
 
-  if (isSeparated) return 2500
-
-  if (isRetired) return 3000
-
-  return 4000
-}
