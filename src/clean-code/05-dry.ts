c78d301a1383365e7a6484aa868db6e1d6087b51
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-08 14:39:50 +0200

    Still no DRY

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -1,7 +1,19 @@
+type Size = '' | 'S' | 'M' | 'L'
 class Product {
-  constructor(public name: string) {}
+  constructor(
+    public name: string = '',
+    public price: number = 0,
+    public size: Size = ''
+  ) {}
+
+  toString() {
+    // Still no DRY
+    if (!this.name.length) throw new Error('name is empty')
+    if (!this.size.length) throw new Error('size is empty')
+    return `${this.name} tiene un precio de ${this.price} y una talla ${this.size}`
+  }
 }
 ;(() => {
-  const bluePants = new Product('Blue Large Pants')
-  console.log(bluePants)
+  const bluePants = new Product('Churton', 0, '')
+  console.log(bluePants.toString())
 })()
