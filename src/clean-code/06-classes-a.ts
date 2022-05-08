928b4371aa5913f516a7f1e77e69d91bcad76407
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-08 17:26:59 +0200

    And now, the cleanest TS way: much cleaner, readable and less error prone

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -2,15 +2,11 @@
   type Gender = 'M' | 'F'
 
   class Person {
-    public name: string
-    public gender: Gender
-    public birthDate: Date
-
-    constructor(name: string, gender: Gender, birthDate: Date) {
-      this.name = name
-      this.gender = gender
-      this.birthDate = birthDate
-    }
+    constructor(
+      public name: string,
+      public gender: Gender,
+      public birthDate: Date
+    ) {}
   }
 
   const newPerson = new Person('Victor', 'M', new Date(1990, 0, 20)) // 20 de Enero de 1990
