55b09c4b3c3b4ebd1ee8fdd763b8eed679314e1c
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-08 13:04:38 +0200

    More readable function using an object: but it actually should be an interface, so much cleaner. But I don't know how to do it well

--- src/clean-code/04-homework.ts
+++ src/clean-code/04-homework.ts
@@ -8,15 +8,13 @@
   // Simplificar esta función
   // switch? Object literal? validar posibles colores
   function getFruitsByColor(color: string): string[] {
-    if (color === 'red') {
-      return ['manzana', 'fresa']
+    const fruitsByColor = {
+      red: ['manzana', 'fresa'],
+      yellow: ['piña', 'banana'],
+      purple: ['moras', 'uvas']
     }
-    if (color === 'yellow') {
-      return ['piña', 'banana']
-    }
-    if (color === 'purple') {
-      return ['moras', 'uvas']
-    }
+    if (fruitsByColor[color]) return fruitsByColor[color]
+
     throw Error('the color must be: red, yellow, purple')
   }
 
