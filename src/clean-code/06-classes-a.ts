0552928a57a8bc3fbdea4d9451479e5a9a0dfd28
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-08 17:44:55 +0200

    Extend class Person to create the class User. With SIX parameters

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -11,4 +11,27 @@
 
   const newPerson = new Person('Victor', 'M', new Date(1990, 0, 20)) // 20 de Enero de 1990
   console.log(newPerson)
+
+  class User extends Person {
+    constructor(
+      public email: string,
+      public role: string,
+      public lastAccess: Date,
+      name: string,
+      gender: Gender,
+      birthDate: Date
+    ) {
+      super(name, gender, birthDate)
+    }
+  }
+
+  const newUser = new User(
+    'email@email.com',
+    'Boss',
+    new Date(Date.now()),
+    'Victor',
+    'M',
+    new Date(1990, 0, 20)
+  )
+  console.log(newUser)
 })()
