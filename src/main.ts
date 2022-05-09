c5eda2c58967b5d1f5f1dea5fc06422bd5b9a05a
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-09 21:06:16 +0200

    Add Liskov principle exercise: for the 3rd principle: L in soLid

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,8 @@ import './code-smells/02-low-coupling' */
 
 // Solid Exercises:
 // import './solid/01-srp'
-import './solid/02-open-close-a'
-
+// import './solid/02-open-close-a'
+import './solid/03-liskov-a'
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
