e6779b2de8b4ba99c8f2fd09126397391a79aabf
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-09 10:11:43 +0200

    Composition of ProductBlock class based on the other three single responsible classes

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -28,17 +28,59 @@
   class ClientNotifier {
     // Example of e-mail config
     // private masterEmail: string = process.env.MASTER_EMAIL
-    notifyClients() {
-      console.log('Enviando correo a los clientes')
+    private masterEmail: string = 'DUMMY_EMAIL_FROM_ENV'
+    notifyClients(masterEmail = this.masterEmail) {
+      console.log(`Enviando correo a los clientes desde ${masterEmail}`)
     }
   }
 
-  const productService = new ProductService()
+  class ProductBlock {
+    private productService: ProductService
+    private clientNotifier: ClientNotifier
+    private cartService: CartService
+    constructor(
+      productService: ProductService,
+      clientNotifier: ClientNotifier,
+      cartService: CartService
+    ) {
+      this.productService = productService
+      this.clientNotifier = clientNotifier
+      this.cartService = cartService
+    }
+    loadProduct(id: number) {
+      // Realiza un proceso para obtener el producto y retornarlo
+      this.productService.loadProduct(id)
+    }
+
+    saveProduct(product: Product) {
+      // Realiza una petición para salvar en base de datos
+      this.productService.saveProduct(product)
+    }
+
+    notifyClients(masterEmail?: string) {
+      this.clientNotifier.notifyClients(masterEmail)
+    }
+    addToCart(productId: number) {
+      this.cartService.addToCart(productId)
+    }
+  }
+
+  /* const productService = new ProductService()
   const cartService = new CartService()
   const clientNotifier = new ClientNotifier()
 
   productService.loadProduct(10)
   productService.saveProduct({ id: 10, name: 'OLED TV' })
   clientNotifier.notifyClients()
-  cartService.addToCart(10)
+  cartService.addToCart(10) */
+  const productBlock = new ProductBlock(
+    new ProductService(),
+    new ClientNotifier(),
+    new CartService()
+  )
+
+  productBlock.loadProduct(10)
+  productBlock.saveProduct({ id: 10, name: 'OLED TV' })
+  productBlock.notifyClients()
+  productBlock.addToCart(10)
 })()
