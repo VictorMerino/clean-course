cc3a9ca7d5c89f3c2c8d8c4603d85bffc3a7481b
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-09 11:39:26 +0200

    Add links to all exercises: uncomment any of them to show them in action in the browser

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 import './style.css'
 
+// Clean Code Exercises
+/* import './clean-code/01-names'
+import './clean-code/02-name-types'
+import './clean-code/03-functions'
+import './clean-code/04-homework'
+import './clean-code/05-dry'
+import './clean-code/06-classes-a'
+import './clean-code/06-classes-b'
+import './clean-code/06-classes-c'
+import './clean-code/07-tarea' */
+
+// Code Smells Exercises:
+/* import './code-smells/01-singleton.js'
+import './code-smells/02-high-coupling'
+import './code-smells/02-low-coupling' */
+
+// Solid Exercises:
+// import './solid/01-srp'
+import './solid/02-open-close-a'
+
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
