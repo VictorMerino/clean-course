5f0d6faa4ecce6c9f844f05061725e7acfd92919
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-11 23:39:21 +0200

    Add two more birds

--- src/solid/04-interface-segregation.ts
+++ src/solid/04-interface-segregation.ts
@@ -2,22 +2,41 @@ interface Bird {
   fly(): void
   eat(): void
   run(): void
+  swim(): void
 }
 
 class Tucan implements Bird {
-  public fly()
-  public eat()
-  public run()
+  public fly() {}
+  public eat() {}
+  public run() {}
+  public swim() {
+    console.log('El que no nada, sahoga')
+  }
 }
 
-class Tucan implements Bird {
-  public fly()
-  public eat()
-  public run()
+class Humminbird implements Bird {
+  public fly() {}
+  public eat() {}
+  public run() {}
+  public swim() {
+    console.log('El que no nada, sahoga')
+  }
 }
 
 class Ostrich implements Bird {
-  public fly()
-  public eat()
-  public run()
+  public fly() {
+    throw new Error('Ave que no vuela, a la cazuela!')
+  }
+  public eat() {}
+  public run() {}
+  public swim() {}
+}
+
+class Penguin implements Bird {
+  public fly() {
+    throw new Error('Ave que no vuela, a la cazuela!')
+  }
+  public eat() {}
+  public run() {}
+  public swim() {}
 }
