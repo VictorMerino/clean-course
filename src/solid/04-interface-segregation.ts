b0a251b7e54af11e624ecbce2d2931bec0120b66
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-11 23:43:43 +0200

    One option is use optional parameters in the interface, but still not the cleanest

--- src/solid/04-interface-segregation.ts
+++ src/solid/04-interface-segregation.ts
@@ -1,41 +1,29 @@
 interface Bird {
-  fly(): void
+  fly?(): void
   eat(): void
   run(): void
-  swim(): void
+  swim?(): void
 }
 
 class Tucan implements Bird {
   public fly() {}
   public eat() {}
   public run() {}
-  public swim() {
-    console.log('El que no nada, sahoga')
-  }
 }
 
 class Humminbird implements Bird {
   public fly() {}
   public eat() {}
   public run() {}
-  public swim() {
-    console.log('El que no nada, sahoga')
-  }
 }
 
 class Ostrich implements Bird {
-  public fly() {
-    throw new Error('Ave que no vuela, a la cazuela!')
-  }
   public eat() {}
   public run() {}
   public swim() {}
 }
 
 class Penguin implements Bird {
-  public fly() {
-    throw new Error('Ave que no vuela, a la cazuela!')
-  }
   public eat() {}
   public run() {}
   public swim() {}
